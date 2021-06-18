1cffff94c43edf5d1952044f03be792c93199253
jc-gaitan <ju.gaitan@udla.edu.co>
2021-06-18 08:19:25 -0500

    Componentes

--- bloc-example/Part 4/lib/repositories/preferences_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bloc_example/models/theme.dart';
-
-abstract class PreferencesRepository {
-  Future<void> setTheme(Theme theme);
-
-  Future<Theme> getTheme();
-}
--- bloc-example/Part 5/lib/presentation/widgets/widgets.dart
+++
@@ -1,1 +0,0 @@
-export 'package:bloc_example/presentation/widgets/action_button.dart';
