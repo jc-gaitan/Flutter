7d27121fbb5be5645b9c8ebb569e4e87f0729e7d
Juan Gaitan <ju.gaitan@udla.edu.co>
2022-05-23 15:48:02 -0500

    ㅤㅤ

--- Flutter Avanzado/getx_example/lib/controllers/controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/state_manager.dart';
-
-class AnimalsController extends GetxController {
-  var list = [].obs;
-
-  add(item) => list.add(item);
-
-  remove(index) => list.removeAt(index);
-}
